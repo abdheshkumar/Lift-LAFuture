bedc3362eb97a4d4d3467ecc723ea4218d63e254
abdhesh <abdhesh@knoldus.com>
2014-12-11 02:56:37 +0530

    Add dammy service

---
+++ src/main/scala/com/ontheserverside/lib/FutureService.scala
@@ -0,0 +1,17 @@
+package com.ontheserverside.lib
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object FutureService {
+
+  /**
+   * Here we are doing server side processing
+   */
+  def processBusinessLogic: Future[String] = {
+    Future {
+      Thread.sleep(2000)
+      "Hello from scala future"
+    }
+  }
+}
